5e2bcfeb6bd4ac10827a30ff635e0f0461b6a89f
Keita Kobayashi <dieubyn1230@gmail.com>
2015-09-09 10:58:06 +0900

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_reputation :likes, source: :user, aggregated_by: :sum
+  mount_uploader :image, ImageUploader
   validates :title, presence: true
 end
